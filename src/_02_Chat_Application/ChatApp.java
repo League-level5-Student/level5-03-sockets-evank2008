ad542600d482cbeb273eb18c919f675e09f26296
Evan Keri <Evan Keri@DESKTOP-2O56U0E.sdcs.local>
2024-12-03 11:29:02 -0800

    removed api key from file

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-//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
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
